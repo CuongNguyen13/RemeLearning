00c7c67db13efd959ff0a264d5e8c2f89ecd7259
CuongNguyen13 <cuong.dev13@gmail.com>
2026-07-23 21:56:40 +0700

    Revert "feat(common): add TopicProgressCalculator for topic-library gating"
    This reverts commit 8ced930561ca89d09c58e1e1ba830ad8af7e2943.

--- RemeLearning/common/src/main/java/com/remelearning/common/library/TopicStatus.java
+++
@@ -1,8 +0,0 @@
-package com.remelearning.common.library;
-
-// Progress state of a single fixed library topic for one learner.
-public enum TopicStatus {
-    LOCKED,
-    IN_PROGRESS,
-    PASSED
-}
